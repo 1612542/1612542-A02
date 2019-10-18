e419a2885f37e2e52677bdd8f213b27344ae58d5
1612542 <tnquoc1998@gmail.com>
2019-10-18 23:25:37 +0700

    1612542-A02 Game

---
+++ Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public GameObject Target;
+
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        Vector3 newPosition = Target.transform.position;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
